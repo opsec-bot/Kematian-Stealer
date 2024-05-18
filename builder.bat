166b9a38965374496083e57d9b6ade2e1755a786
KDot227 <damndc227@gmail.com>
2024-05-17 19:17:25 -0500

    make it clear for people building

--- builder.bat
+++ builder.bat
@@ -2,6 +2,16 @@
 
 cd /d %~dp0
 
+echo THIS IS IF YOU WANT TO BUILD THE BUILDER YOURSELF.
+echo THE BUILDER IS ALREADY BUILT BY GITHUB AND IS 100% OPEN SOURCE.
+echo THE BUILT BUILDER IS IN THE RELEASES ON THE GITHUB.
+echo https://github.com/ChildrenOfYahweh/Kematian-Stealer/releases/tag/Builder
+echo.
+echo.
+echo Press any key to continue building the builder yourself.
+
+pause
+
 go version
 if %errorlevel% neq 0 (
     echo "Go is not installed or not in PATH. Either install it or use the prebuilt builder image."
